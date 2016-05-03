b717c8280b6ebcaa5dc5c85653380f6949f8b557
liushuishang <liushuishang@gmail.com>
2016-05-04 00:23:46 +0800

    添加web管理端工程

---
+++ yaycrawler.admin/src/main/java/yaycrawler/admin/Application.java
@@ -0,0 +1,26 @@
+package yaycrawler.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"yaycrawler"})
+@EntityScan(basePackages = {"yaycrawler.common.domain"})
+@EnableJpaRepositories(basePackages = {"yaycrawler.common.dao.repositories"})
+public class Application  extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
