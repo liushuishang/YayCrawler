d97b7e9379a8eae4657413908db54577e755ce9c
guibiaoguo <guoa1234@163.com>
2016-06-14 18:45:57 +0800

    更改了master的redis读取频度，队列重新整合，admin更改上传文件大小

--- yaycrawler.admin/src/main/java/yaycrawler/admin/Application.java
+++ yaycrawler.admin/src/main/java/yaycrawler/admin/Application.java
@@ -1,11 +1,14 @@
 package yaycrawler.admin;
 
 import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.orm.jpa.EntityScan;
@@ -13,34 +16,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.servlet.MultipartConfigElement;
+
 @SpringBootApplication
 @ImportResource(locations = {"classpath*:spring/*.xml"})
 @EntityScan(basePackages = {"yaycrawler.dao.domain"})
 @EnableJpaRepositories(basePackages = {"yaycrawler.dao.repositories"})
 public class Application {
-
-    @Value("${spring.multipart.maxFileSize}")
-    private int maxPostSize;
-
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
-        return new EmbeddedServletContainerCustomizer() {
-            @Override
-            public void customize(ConfigurableEmbeddedServletContainer container) {
-                if (container instanceof TomcatEmbeddedServletContainerFactory) {
-                    TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
-                    TomcatConnectorCustomizer tomcatConnectorCustomizer = new TomcatConnectorCustomizer() {
-                        @Override
-                        public void customize(Connector connector) {
-                            connector.setMaxPostSize(maxPostSize);
-                        }
-                    };
-                }
-            }
-        };
     }
 }
